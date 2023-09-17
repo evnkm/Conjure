2e830090f535f98ab6c60e366298eca9f24d8149
azhang1001 <alexander.zhang2004@gmail.com>
2023-09-16 23:12:05 -0400

    added menu bar and echo chat bot

--- conjure.py
+++ conjure.py
@@ -1,17 +1,27 @@
+from views import home, about, options, configuration
+import utils as utl
 import streamlit as st
-import pandas as pd
-import numpy as np
-from io import StringIO
-from PIL import Image
+
+# st.set_page_config(layout="wide", page_title='Navbar sample')
 
 
-st.title('Conjure')
+st.set_option('deprecation.showPyplotGlobalUse', False)
+utl.inject_custom_css()
+utl.navbar_component()
 
 
-uploaded_files = st.file_uploader("Choose file(s)", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    if uploaded_file is not None:
-        img = Image.open(uploaded_file)
+def navigation():
+    route = utl.get_current_route()
+    if route == "home":
+        home.load_view()
+    elif route == "about":
+        about.load_view()
+    elif route == "options":
+        options.load_view()
+    elif route == "configuration":
+        configuration.load_view()
+    elif route == None:
+        home.load_view()
 
-title = st.text_input(
-    'Enter a prompt', 'Show me all images with a sky in the background.')
+
+navigation()
---
+++ paths.py
@@ -0,0 +1,9 @@
+NAVBAR_PATHS = {
+    'HOME': 'home',
+    'ABOUT': 'about',
+}
+
+SETTINGS = {
+    'OPTIONS': 'options',
+    'CONFIGURATION': 'configuration'
+}
