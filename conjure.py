59f7c7dbf216fab73f7da770a172a7c08da43eeb
azhang1001 <alexander.zhang2004@gmail.com>
2023-09-16 23:36:26 -0400

    update

--- conjure.py
+++ conjure.py
@@ -2,8 +2,6 @@ from views import home, about, options, configuration
 import utils as utl
 import streamlit as st
 
-# st.set_page_config(layout="wide", page_title='Navbar sample')
-
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 utl.inject_custom_css()
